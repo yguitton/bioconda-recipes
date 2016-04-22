617f3085c83460082c9d3c56a71e3d7e9e9f5970
Tom Smith <thomas.smith2@dpag.ox.ac.uk>
2016-04-22 19:08:06 +0100

    debug build.sh sed command

--- recipes/umi_tools/build.sh
+++ recipes/umi_tools/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i -e 's/=install_requires,/#/g' -e 's/install_requires#/#/g' setup.py
+sh -c "sed -i -e 's/=install_requires,/#/g' -e 's/install_requires#/#/g' setup.py"
 ${PYTHON} setup.py install
